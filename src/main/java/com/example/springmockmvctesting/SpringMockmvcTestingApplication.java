d791ea3c261512f50857ffdc2d7cbab84f26afd3
Kleber Vales <ksvales@outlook.pt>
2025-08-14 13:43:29 -0300

    chore: update message

--- src/main/java/com/example/springmockmvctesting/SpringMockmvcTestingApplication.java
+++ src/main/java/com/example/springmockmvctesting/SpringMockmvcTestingApplication.java
@@ -9,3 +9,12 @@ public class SpringMockmvcTestingApplication {
         SpringApplication.run(SpringMockmvcTestingApplication.class, args);
     }
 }
+
+/*
+
+    - Criar testes simulando requisições HTTP sem subir servidor real. 
+    - Usar @AutoConfigureMockMvc + injeção de MockMvc. 
+    - Validar status, corpo e headers da resposta. 
+    - criar o terceiro projeto com estrutura de pastas e codigo dos arquivos
+    
+*/
